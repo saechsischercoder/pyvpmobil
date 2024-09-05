db71cff9dfc21fdc4ed18ba5bdfad4664dc0aba0
BlueSchnabeltier <97361820+BlueSchnabeltier@users.noreply.github.com>
2024-09-05 17:16:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name="pyVPMobil",
+setup(name="pyvpmobil",
     version="1.1",
     description="An unoffical python package to interact with the API of the german timetable service VpMobil/Stundenplan24",
     author="BlueSchnabeltier",
     author_email="finn.ueschner@icloud.com",
-    url="https://github.com/BlueSchnabeltier/pyVPMobil",
-    packages=["pyVPMobil"],
+    url="https://github.com/BlueSchnabeltier/pyvpmobil",
+    packages=["pyvpmobil"],
     install_requires=["datetime", "requests", "xmltodict"],
 )
