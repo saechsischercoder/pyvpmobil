432216995e128a0a7682348307e14d75d8e0eb23
sächsischer coder <97361820+saechsischercoder@users.noreply.github.com>
2025-06-13 16:50:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name="pyvpmobil",
     description="Python API client for German school timetable services VpMobil/Stundenplan24 with comprehensive data access",
-    author="BlueSchnabeltier",
+    author="saechsischercoder",
     url="https://github.com/saechsischercoder/pyvpmobil",
     packages=["pyvpmobil"],
     install_requires=["datetime", "requests", "xmltodict"],
